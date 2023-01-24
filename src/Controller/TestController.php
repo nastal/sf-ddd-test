cc29108ebfb590f4fd32d9fd7dee977670fcb1da
AlexN <a.nastasins@marine-digital.com>
2023-01-25 00:04:21 +0200

    Logger and messenger packages added

---
+++ src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use PhpParser\ErrorHandler\Throwing;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(LoggerInterface $logger): JsonResponse
+    {
+
+        $logger->info('hello worlddd');
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TestController.php',
+        ]);
+    }
+}
