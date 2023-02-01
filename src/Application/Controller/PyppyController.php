b1cce6c4b8b05e13bcbfd0b861335269e6fa5549
AlexN <a.nastasins@marine-digital.com>
2023-02-01 15:46:30 +0200

    Doctrine exploring and sandboxing

---
+++ src/Application/Controller/PyppyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Controller;
+
+use Application\Entity\AgreeablePuppy;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PyppyController extends AbstractController
+{
+    #[Route('/controller/pyppy', name: 'app_controller_pyppy')]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+
+        $entityManager = $doctrine->getManager();
+
+        $puppy = new AgreeablePuppy();
+        $puppy->setName('Nani');
+
+        $entityManager->persist($puppy);
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Puppy ' . $puppy->getName() . ' is saved',
+            'id' => $puppy->getId(),
+        ]);
+    }
+}
---
+++ src/Application/Repository/AgreeablePuppyRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Application\Repository;
+
+use Application\Entity\AgreeablePuppy;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<AgreeablePuppy>
+ *
+ * @method AgreeablePuppy|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AgreeablePuppy|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AgreeablePuppy[]    findAll()
+ * @method AgreeablePuppy[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AgreeablePuppyRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, AgreeablePuppy::class);
+    }
+
+    public function save(AgreeablePuppy $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(AgreeablePuppy $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return AgreeablePuppy[] Returns an array of AgreeablePuppy objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?AgreeablePuppy
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
