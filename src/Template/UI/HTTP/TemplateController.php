19165756f00dfb19de7576202450f678397d3505
AlexN <a.nastasins@marine-digital.com>
2023-02-22 10:14:01 +0200

    Template service WIP

--- src/Template/UI/HTTP/TemplateController.php
+++ src/Template/UI/HTTP/TemplateController.php
@@ -4,7 +4,6 @@ namespace App\Template\UI\HTTP;
 
 use App\Template\Application\TemplateRenderer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
